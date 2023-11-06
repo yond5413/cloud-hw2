7ae7f47e2af2181fa23a700e533ee643c7819f5d
yond5413 <87887705+yond5413@users.noreply.github.com>
2023-11-06 17:37:59 -0500

    Frontend updates

--- assets/js/request.js
+++ assets/js/request.js
@@ -8,7 +8,7 @@ function callPutPhotoApi(file,filename,labels) {
         'Content-Type': 'text/base64'
 
     };
-    var body = {"body":file}
+    var body = {file}
     /*- in method reques add Content-Type: text/base64;
      custom label too
      file co
@@ -29,35 +29,77 @@ function callPutPhotoApi(file,filename,labels) {
       throw error; // Propagate the error for further handling if needed
   });
   }
+
+  function callSearchPhotosApi(q) {
+  //// have nto implemented yet!
+    return sdk.searchGet({},{},{})
+    .then(function (result) {
+      // Handle the successful response
+      console.log("API response:", result);
+      return result;
+  })
+  .catch(function (error) {
+      // Handle errors
+      console.error("API error:", error);
+      throw error; // Propagate the error for further handling if needed
+  });
+  }
+///////////////////////////////
+const convertBase64 = (file) => {
+  return new Promise((resolve, reject) => {
+      const fileReader = new FileReader();
+      fileReader.readAsDataURL(file);
+
+      fileReader.onload = () => {
+          resolve(fileReader.result);
+      };
+
+      fileReader.onerror = (error) => {
+          reject(error);
+      };
+  });
+};
 /////////event-stuff///////////
 document.addEventListener('DOMContentLoaded', function () {
     const imageForm = document.getElementById('image-form');
     const searchForm = document.getElementById('search-form');
     const searchResults = document.getElementById('search-results');
     const uploadedImage = document.getElementById('uploaded-image');
-
+    var base64String = null;
     // Handle image upload form submission
     imageForm.addEventListener('submit', function (event) {
         event.preventDefault();
-
+        console.log(event)
         const imageInput = document.getElementById('image-input');
         const labelInput = document.getElementById('label-input');
-
         const formData = new FormData(imageForm);
         console.log(labelInput.value);
-        // You can send the form data to the server for processing here
-        // For now, we'll just display the selected image
         const selectedImage = imageInput.files[0];
         ///////////////////////////////////
-        const binaryData =selectedImage ;//e.target.result;
+        /*const binaryData =event.target.result;//selectedImage ;//e.target.result;
         const base64String = btoa(binaryData);
-        console.log('Base64 String:', base64String);
+        console.log('Base64 String:', base64String);*/
+       // const binaryData = reader.result.split(',')[1];
+       //base64String = btoa(binaryData);
+        //base64String = btoa(selectedImage);
+        //console.log('Base64 String:', base64String);
+        //var filename = selectedImage.name;
+        //uploadedImage.src = URL.createObjectURL(selectedImage);
+        //callPutPhotoApi(base64String,filename,labelInput.value);
+        var reader = new FileReader();
+        reader.onload = function() {
+            base64String = btoa(reader.result);
+            var filename = selectedImage.name;
+            uploadedImage.src = URL.createObjectURL(selectedImage);
+            console.log("hello: "+base64String)
+            callPutPhotoApi(base64String,filename,labelInput.value);
+        };
+        reader.readAsBinaryString(selectedImage);
+        //reader.readAsDataURL(selectedImage);
+        /// also returns base64 str
         ///////////////////////////////////
-        console.log(selectedImage);
-        console.log(selectedImage.name);
-        var filename = selectedImage.name;
-        uploadedImage.src = URL.createObjectURL(selectedImage);
-        callPutPhotoApi(base64String,filename,labelInput.value);
+        //console.log(selectedImage);
+        //console.log(selectedImage.name);
     });
 
     // Handle search form submission
