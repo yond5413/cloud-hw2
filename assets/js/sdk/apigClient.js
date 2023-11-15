21603683bad3e39f19b058c40100a73af1b4f51d
yond5413 <87887705+yond5413@users.noreply.github.com>
2023-11-15 18:32:52 -0500

    update

--- assets/js/sdk/apigClient.js
+++ assets/js/sdk/apigClient.js
@@ -53,7 +53,7 @@ apigClientFactory.newClient = function (config) {
 
     
     // extract endpoint and path from url
-    var invokeUrl = 'https://bw2i7kxmj3.execute-api.us-east-1.amazonaws.com/prod';
+    var invokeUrl = 'https://bw2i7kxmj3.execute-api.us-east-1.amazonaws.com/copium';
     var endpoint = /(^https?:\/\/[^\/]+)/g.exec(invokeUrl)[1];
     var pathComponent = invokeUrl.substring(endpoint.length);
 
